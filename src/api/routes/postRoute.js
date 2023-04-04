4bd5999e0f43c6496a20cc7ad9bec8525e094e53
atacanjacques <atacan.jacques@gmail.com>
2023-04-04 15:51:49 +0200

    Express routing

---
+++ src/api/routes/postRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const postController = require('../controllers/postController')
+
+// /posts
+router
+    .route('/')
+    .get(postController.listAllPosts)
+    .post(postController.createAPost);
+
+// /posts/:id_post
+// router
+//     .route('/:id_post')
+//     .get(postController.getAPost)
+//     .put(postController.updateAPost)
+//     .delete (postController.deleteAPost);
+
+module.exports = router;
